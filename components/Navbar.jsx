bd6d44cbfc2ecd1afc0928d4c73b5e3f8c46ae97
Ishan Shah <ishanshah2612@gmail.com>
2023-07-01 15:35:29 +0530

    state_context+cart_page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React , { useContext } from 'react';
 import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai';
+import { StateContext  } from '../context/StateContext';
+import MyCart from './MyCart';
+
 
 const Navbar = () => {
+  const { showCart , setShowCart , totalQuantities } = useContext(StateContext);
+
   return (
     <div className="navbar-container">
       <p className="logo">
          <Link href="/">AudioVerse Headphones</Link>
       </p>
 
-      <button type="button" className="cart-icon" onClick="" >
+      <button type="button" className="cart-icon" onClick= {() =>setShowCart(true)} >
         <AiOutlineShopping/>
-        <span className="cart-item-qty">1</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
+      {showCart && <MyCart/>}
     </div>
   )
 }
